c0734d7ce990490c7836fc9cd51242e75d8e1641
ikhela-04KK <102194271+ikhela-04KK@users.noreply.github.com>
2023-04-26 23:54:10 +0000

    new modif

--- jindex.js
+++ jindex.js
@@ -1,19 +1,29 @@
 // modifier le h1 à chaque fois que l'on clique que l'on tape au clavier 
 
+// essayer de faire ajax avec jquery 
+
 let write = ""
-$(document).on("keydown",(function(e){
-  if(e.key.trim() === "Backspace"){
-    // lorssqu'on appuie sur backspace il supprime la derniere lettre à chaque fois 
-    write = write.slice(0,-1); // permet de supprimer le dernier élément de la chaîne
-  }else{
-    write +=e.key    
+$(document).on({
+  keydown: function(e){
+    if(e.key.trim() === "Backspace"){
+      // lorssqu'on appuie sur backspace il supprime la derniere lettre à chaque fois 
+      write = write.slice(0,-1); // permet de supprimer le dernier élément de la chaîne
+    }
+    else{
+      write +=e.key;    
+    }
+    $("h1").text(write);
+  },
+
+  keypress: function(e){
+    write +=e.key
   }
-  $("h1").text(write)
-}))
+    $("h1").text(write);
+  
 
 
 // animer les éléments 
 // essayer de voir plutard si les keyframes passe ici
-$("button").on("click",function(e){
+$("button").on("click",function(){
   $("h1").slideToggle().fadeOut().animate({opacity:0.5})
 });
